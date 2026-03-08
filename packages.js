fde5705afc0f7aa83536ebe7444b4c799dee8e41
FFox3 <ffox211310@gmail.com>
2026-03-08 22:36:07 +0900

    Create packages.js

---
+++ packages.js
@@ -0,0 +1,23 @@
+let packages = {
+
+python3:{
+name:"python3",
+description:"Python interpreter (education version)",
+install:function(){
+
+alert("Python3 installed!\n(education simulator)");
+
+}
+},
+
+nano:{
+name:"nano",
+description:"Simple text editor",
+install:function(){
+
+alert("Nano installed!");
+
+}
+}
+
+};
