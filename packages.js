badc37898cf59cbbfb5ac641313f6b5a01fa09ee
FFox3 <ffox211310@gmail.com>
2026-03-08 23:23:27 +0900

    Update packages.js

--- packages.js
+++ packages.js
@@ -2,22 +2,32 @@ let packages = {
 
 python3:{
 name:"python3",
-description:"Python interpreter (education version)",
+description:"Python interpreter",
+
 install:function(){
 
-alert("Python3 installed!\n(education simulator)");
+installedPackages["python3"]=true;
+
+alert("Python3 installed!");
 
 }
+
 },
 
 nano:{
 name:"nano",
-description:"Simple text editor",
+description:"Text editor",
+
 install:function(){
+
+installedPackages["nano"]=true;
 
 alert("Nano installed!");
 
 }
+
 }
 
 };
+
+let installedPackages={};
